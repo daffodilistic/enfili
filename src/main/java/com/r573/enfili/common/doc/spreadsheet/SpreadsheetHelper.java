45653dcf3062a78fce5da3a2c9cde4d418999955
Ryan Ho <ryanhosp@gmail.com>
2014-11-24 16:56:00 +0800

    Updated POI library and new methods for SpreadsheetHelper

--- src/main/java/com/r573/enfili/common/doc/spreadsheet/SpreadsheetHelper.java
+++ src/main/java/com/r573/enfili/common/doc/spreadsheet/SpreadsheetHelper.java
@@ -18,11 +18,65 @@
 
 package com.r573.enfili.common.doc.spreadsheet;
 
+import java.util.Iterator;
+
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class SpreadsheetHelper {
 	private static final char FIRST_ALPHA_CHAR = 65;
 	private static final int NUM_ALPHA_CHARS = 26;
+	
+	/**
+	 * Creates a XSSFWorkbook with 1 default sheet "Sheet1" and returns the Worksheet
+	 * This is for simple temporary spreadsheets for calculation purpose
+	 * 
+	 * @return
+	 */
+	public static Sheet createSimpleWorksheet(int numCols, int numRows){
+		Workbook workbook = new XSSFWorkbook();
+		Sheet sheet = workbook.createSheet("Sheet1");
+		for(int i=0;i<numRows;i++){
+			Row row = sheet.createRow(i);
+			for(int j=0;j<numCols;j++){
+				row.createCell(j);
+			}
+		}
+		return sheet;
+	}
+	
+	public static void setCellFormula(Sheet sheet, int colId, int rowId, String formula){
+		getCell(sheet, colId, rowId).setCellFormula(formula);
+	}
+	public static void setCellValue(Sheet sheet, int colId, int rowId, int value){
+		getCell(sheet, colId, rowId).setCellValue(value);
+	}
+	public static void setCellValue(Sheet sheet, int colId, int rowId, double value){
+		getCell(sheet, colId, rowId).setCellValue(value);
+	}
+	
+	public static Cell getCell(Sheet sheet, int colId, int rowId){
+		Row row = sheet.getRow(rowId);
+		return row.getCell(colId);
+	}
+	
+	public static void calculate(Sheet sheet){
+        FormulaEvaluator evaluator = sheet.getWorkbook().getCreationHelper().createFormulaEvaluator();
+        for (Iterator<Row> rowIterator = sheet.rowIterator(); rowIterator.hasNext();)  {
+        	Row row = rowIterator.next();
+    		for (Iterator<Cell> cellIterator = row.cellIterator(); cellIterator.hasNext();) {
+    			Cell cell = cellIterator.next();
+    			if (cell != null && cell.getCellType() == XSSFCell.CELL_TYPE_FORMULA) {
+    				evaluator.evaluateInCell(cell);                 
+    			}
+    		}
+        }
+	}
 	
 	public static boolean isCellEmpty(Cell cell) {
 		if (cell == null) {
@@ -35,9 +89,32 @@ public class SpreadsheetHelper {
 			}
 		}
 	}
-
-	public static int getIntCellValue(Cell cell) {
+	public static int getCellValueAsInt(Sheet sheet, int colId, int rowId) {
+		return getCellValueAsInt(getCell(sheet, colId, rowId));
+	}
+	public static long getCellValueAsLong(Sheet sheet, int colId, int rowId) {
+		return getCellValueAsInt(getCell(sheet, colId, rowId));
+	}
+	public static double getCellValueAsDouble(Sheet sheet, int colId, int rowId) {
+		return getCellValueAsDouble(getCell(sheet, colId, rowId));
+	}
+	public static float getCellValueAsFloat(Sheet sheet, int colId, int rowId) {
+		return getCellValueAsFloat(getCell(sheet, colId, rowId));
+	}
+	public static int getCellValueAsInt(Cell cell) {
 		int val = (int) Math.round(cell.getNumericCellValue());
+		return val;
+	}
+	public static long getCellValueAsLong(Cell cell) {
+		long val = Math.round(cell.getNumericCellValue());
+		return val;
+	}
+	public static double getCellValueAsDouble(Cell cell) {
+		double val = cell.getNumericCellValue();
+		return val;
+	}
+	public static float getCellValueAsFloat(Cell cell) {
+		float val = (float)cell.getNumericCellValue();
 		return val;
 	}
 
